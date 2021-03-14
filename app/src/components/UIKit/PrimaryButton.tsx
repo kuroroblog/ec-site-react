f68cc390af79118e03519115e77c85b519ae521a
tocchimittsu <arimagedon21@gmail.com>
2021-03-15 07:46:09 +0900

    修正

--- app/src/components/UIKit/PrimaryButton.tsx
+++ app/src/components/UIKit/PrimaryButton.tsx
@@ -1,16 +1,19 @@
 import Button from '@material-ui/core/Button'
-import { makeStyles } from '@material-ui/styles'
+import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   button: {
     backgroundColor: '#4dd0e1',
     color: '#000',
-    fontSize: 16,
+    fontSize: 12,
     height: 48,
     marginButton: 16,
     width: 256,
+    [theme.breakpoints.down('sm')]: {
+      fontsize: 12,
+    },
   },
-})
+}))
 
 const PrimaryButton = (props: { onClick: any; label: string }) => {
   const classes = useStyles()
